2dc1b5c2df206242eb950e4837a7dd1fa824a0a8
Roberto Zagni <rz70rz70@gmail.com>
2016-01-13 19:52:32 +0200

    Asks how many in top list and eeturns top words by frequency

--- Chapter10/wordFreq.py
+++ Chapter10/wordFreq.py
@@ -1,5 +1,15 @@
-fileName = raw_input("Enter file:")
-if len(fileName) < 1 : fileName = "../Chapter8/romeo.txt"
+DEFAULT_FILE = "../Chapter8/romeo.txt"
+fileName = raw_input("Enter file name [" + DEFAULT_FILE + "]:")
+if len(fileName) < 1 : fileName = DEFAULT_FILE
+
+DEFAULT_NUM = 5
+num = raw_input("Enter number for Top selection [" + str(DEFAULT_NUM) + "]:")
+if len(num) < 1 : num = DEFAULT_NUM
+else:
+    try: num = int(num)
+    except: num = DEFAULT_NUM
+
+
 fh = open(fileName)
 
 wordFreq = dict()
@@ -13,4 +23,4 @@ byWord = sorted(wordFreq.items())
 print "Sorted by word\n", byWord, "\n"
 
 byFreq = sorted([ (count, wrd) for wrd,count in wordFreq.items()], reverse=True)
-print "Sorted by frequency\n",byFreq, "\n"
+print "Top", num, "sorted by frequency\n",byFreq[:num], "\n"
